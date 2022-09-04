9c3184d7217e629732109284918d88e1aa4f74eb
IvanKovalev <deformgame@gmail.com>
2022-09-04 08:02:09 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (num > 0)
+    {
+        sum = sum + num % 10;
+        num =  num / 10;
+    }
+Console.WriteLine($"Сумма всех чисел в введенном числе равна {sum} .");
