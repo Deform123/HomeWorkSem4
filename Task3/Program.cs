53ac706fecca24a9dd72629ddea652a04ebaf055
IvanKovalev <deformgame@gmail.com>
2022-09-04 08:19:34 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+Console.WriteLine("Введите воличество чисел в массиве : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [n];
+
+for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next();
+        Console.Write($"{array [i] + " "}");
+    }
